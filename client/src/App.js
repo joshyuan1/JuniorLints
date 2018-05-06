816926917ec27a37e2eaf37c5801f06301180111
Nick Jaczko <njaczko@middlebury.edu>
2018-05-05 22:46:21 -0400

    has bio

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import {Well} from 'react-bootstrap';
+import styled from 'styled-components';
 import './App.css';
+import logo from './logo.png';
 
 import FileUpload from './FileUpload';
 import Viewer from './Viewer';
 import Loading from './Loading';
+
+const Button = styled.button`
+background-color: DarkGray;
+color: black;
+font-size: 12px;
+padding: 10px 15px;
+margin: 15px;
+border: none;
+cursor: pointer;
+border-radius: 3px;
+text-align: center;
+`;
+
+
 
 class App extends Component {
   constructor() {
@@ -13,6 +29,7 @@ class App extends Component {
     this.state = {
       pyCode: null,
       linterOutput: null,
+      viewBio: null,
       mode: 'upload',
     };
   }
@@ -33,6 +50,7 @@ class App extends Component {
 
   render() {
     let comp;
+    let bio;
     if (this.state.mode === 'upload') {
       comp = (
         <FileUpload
@@ -55,6 +73,36 @@ class App extends Component {
         <Loading />
       );
     }
+    if (this.state.viewBio){
+      bio = (
+        <div align="left">
+          <Button onClick={() => this.setState({viewBio: false})} >
+            What is JuniorLints?
+          </Button>
+          <div>
+          <Well >
+            <p>
+              "Linting" is the act of checking a piece of code for errors and bad style. "Static analysis" is code analysis that is performed without actually executing the code. <strong>JuniorLints</strong> will check your python code for some basic problems and style conventions. <strong>JuniorLints</strong> might not catch everything, but it will probably help!
+            </p>
+            <input
+              type="image"
+              src={logo}
+              alt="logo"
+              width="300px"
+            />
+          </Well>
+          </div>
+        </div>
+      );
+    } else {
+      bio = (
+        <div align="left">
+          <Button onClick={() => this.setState({viewBio: true})} >
+            What is JuniorLints?
+          </Button>
+        </div>
+      )
+    }
 
     return (
       <div className="App">
@@ -63,7 +111,8 @@ class App extends Component {
           <h3 className="App-subtitle">Online Static Analysis for Python</h3>
         </header>
         {comp}
-      </div>
+        {bio}
+        </div>
     );
   }
 }
--- client/src/Viewer.js
+++ client/src/Viewer.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+  import styled from 'styled-components';
 import React, { Component } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/styles/hljs';
